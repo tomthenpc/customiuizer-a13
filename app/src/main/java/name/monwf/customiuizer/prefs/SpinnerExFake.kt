c32fb300da0c1e3611c0bcf7b05c423ddaf25a78
Devin Agent <devin@local>
2026-07-27 02:14:32 +0800

    refactor: migrate ListPreferenceEx, DropDownPreferenceEx, SeekBarPreference, SpinnerEx, SpinnerExFake to Kotlin

---
+++ app/src/main/java/name/monwf/customiuizer/prefs/SpinnerExFake.kt
@@ -0,0 +1,43 @@
+package name.monwf.customiuizer.prefs
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.util.Pair
+import name.monwf.customiuizer.utils.AppHelper
+
+class SpinnerExFake(context: Context, attrs: AttributeSet) : SpinnerEx(context, attrs) {
+
+    private var value: String? = null
+    private val others = ArrayList<Pair<String, String>>()
+
+    init {
+        others.clear()
+    }
+
+    fun setValue(`val`: String) {
+        value = `val`
+    }
+
+    fun getValue(): String? = value
+
+    fun addValue(key: String, `val`: String?) {
+        var sVal = `val`
+        if (sVal == null) sVal = AppHelper.getStringOfAppPrefs(key, null)
+        if (sVal != null) others.add(Pair(key, sVal))
+    }
+
+    fun addValue(key: String, `val`: Intent?) {
+        val sVal = `val`?.toUri(0) ?: AppHelper.getStringOfAppPrefs(key, null)
+        if (sVal != null) others.add(Pair(key, sVal))
+    }
+
+    fun applyOthers() {
+        if (others.isEmpty()) return
+        val editor = AppHelper.appPrefs.edit()
+        for (pref in others) {
+            editor.putString(pref.first, pref.second)
+        }
+        editor.apply()
+    }
+}
