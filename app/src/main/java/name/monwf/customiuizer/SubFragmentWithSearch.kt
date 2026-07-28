0b466ec01c93f49f69df77e1eee9bbd190d7e18e
Devin Agent <devin@local>
2026-07-28 14:53:45 +0800

    refactor(kotlin): migrate SubFragmentWithSearch and ActivitySelector to Kotlin

---
+++ app/src/main/java/name/monwf/customiuizer/SubFragmentWithSearch.kt
@@ -0,0 +1,107 @@
+package name.monwf.customiuizer
+
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import name.monwf.customiuizer.utils.AppDataAdapter
+import name.monwf.customiuizer.utils.Helpers
+import name.monwf.customiuizer.utils.LockedAppAdapter
+import name.monwf.customiuizer.utils.PrivacyAppAdapter
+import name.monwf.customiuizer.utils.ResolveInfoAdapter
+
+open class SubFragmentWithSearch : SubFragment() {
+
+    @JvmField
+    var listView: ListView? = null
+    private var searchView: View? = null
+    private var isSearchFocused = false
+    private var textInput: TextView? = null
+
+    fun setActionModeStyle(searchView: View?) {
+        val ctx = getValidContext() ?: return
+        val isNight = Helpers.isNightMode(ctx)
+        if (searchView != null) try {
+            searchView.setSaveFromParentEnabled(false)
+            val inputIcon = searchView.findViewById<ImageView>(R.id.inputIcon)
+            inputIcon?.setImageResource(
+                resources.getIdentifier(
+                    if (isNight) "edit_text_search_dark" else "edit_text_search",
+                    "drawable",
+                    "miui"
+                )
+            )
+            inputIcon?.setImageTintList(
+                ColorStateList.valueOf(
+                    resources.getColor(R.color.color_on_surface_variant, ctx.theme)
+                )
+            )
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val view = view ?: return
+
+        searchView = view.findViewById(R.id.searchView)
+        setActionModeStyle(searchView)
+        textInput = searchView?.findViewById(android.R.id.input)
+
+        textInput?.setOnFocusChangeListener { _, hasFocus ->
+            isSearchFocused = hasFocus
+        }
+        textInput?.setOnClickListener { v ->
+            isSearchFocused = v.hasFocus()
+        }
+        textInput?.setOnEditorActionListener { v, _, event ->
+            if (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER) {
+                val act = activity as? AppCompatActivity
+                if (act != null) Helpers.hideKeyboard(act, v)
+                listView?.requestFocus()
+                true
+            } else {
+                false
+            }
+        }
+        textInput?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                applyFilter(s.toString().trim())
+            }
+        })
+
+        listView = view.findViewById(android.R.id.list)
+        listView?.setOnTouchListener { v, event ->
+            if (isSearchFocused) {
+                isSearchFocused = false
+                val act = activity as? AppCompatActivity
+                if (act != null) Helpers.hideKeyboard(act, v)
+            }
+            false
+        }
+    }
+
+    protected fun applyFilter(filter: String) {
+        val adapter = listView?.adapter ?: return
+        when (adapter) {
+            is AppDataAdapter -> adapter.filter.filter(filter)
+            is PrivacyAppAdapter -> adapter.filter.filter(filter)
+            is LockedAppAdapter -> adapter.filter.filter(filter)
+            is ResolveInfoAdapter -> adapter.filter.filter(filter)
+            else -> {}
+        }
+    }
+}
---
+++ app/src/main/java/name/monwf/customiuizer/subs/ActivitySelector.kt
@@ -0,0 +1,103 @@
+package name.monwf.customiuizer.subs
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import name.monwf.customiuizer.R
+import name.monwf.customiuizer.SubFragmentWithSearch
+import name.monwf.customiuizer.mods.GlobalActions
+import name.monwf.customiuizer.utils.AppData
+import name.monwf.customiuizer.utils.AppDataAdapter
+import name.monwf.customiuizer.utils.Helpers
+import java.util.ArrayList
+
+class ActivitySelector : SubFragmentWithSearch() {
+
+    private var pkg: String? = null
+    private var key: String? = null
+    private var user = 0
+    private val activities = ArrayList<AppData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        this.padded = false
+        super.onCreate(savedInstanceState)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val args = arguments ?: return
+        key = args.getString("key")
+        pkg = args.getString("package")
+        user = args.getInt("user")
+
+        val process = Runnable {
+            if (activities.size == 0) {
+                Toast.makeText(activity, R.string.no_activities_found, Toast.LENGTH_SHORT).show()
+                finish()
+                return@Runnable
+            }
+            listView?.adapter = AppDataAdapter(
+                activity?.applicationContext,
+                activities,
+                Helpers.AppAdapterType.Activities,
+                null
+            )
+            listView?.setOnItemClickListener { parent, _, position, _ ->
+                val appData = (parent.adapter as? AppDataAdapter)?.getItem(position) ?: return@setOnItemClickListener
+                val intent = Intent(activity, this.javaClass).apply {
+                    putExtra("activity", "${appData.pkgName}|${appData.actName}")
+                    putExtra("user", user)
+                }
+                targetFragment?.onActivityResult(targetRequestCode, Activity.RESULT_OK, intent)
+                finish()
+            }
+            listView?.setOnItemLongClickListener { parent, _, position, _ ->
+                val appData = (parent.adapter as? AppDataAdapter)?.getItem(position) ?: return@setOnItemLongClickListener true
+                val componentPkg = appData.pkgName ?: ""
+                val componentAct = appData.actName ?: ""
+                val intent = Intent(activity, this.javaClass).apply {
+                    component = ComponentName(componentPkg, componentAct)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+                    putExtra("user", user)
+                }
+                val bIntent = Intent(GlobalActions.ACTION_PREFIX + "LaunchIntent").apply {
+                    putExtra("intent", intent)
+                }
+                activity?.sendBroadcast(bIntent)
+                true
+            }
+            view?.findViewById<View>(R.id.am_progressBar)?.visibility = View.GONE
+        }
+
+        Thread {
+            try {
+                Thread.sleep(animDur.toLong())
+            } catch (_: Throwable) {}
+            try {
+                val act = activity ?: return@Thread
+                activities.clear()
+                val pm = act.packageManager
+                val pi = pm.getPackageInfo(pkg ?: "", PackageManager.GET_ACTIVITIES)
+                pi.activities?.forEach { info ->
+                    val appData = AppData().apply {
+                        pkgName = pkg
+                        actName = info.name ?: ""
+                        label = info.loadLabel(pm).toString()
+                        enabled = info.enabled
+                    }
+                    activities.add(appData)
+                }
+                act.runOnUiThread(process)
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+}
